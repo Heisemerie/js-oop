1180820dd7b60ec48ddc58918f4641f8023755c6
Heisemerie <ejike162@gmail.com>
2025-03-22 09:32:34 +0100

    getting started

---
+++ index.js
@@ -0,0 +1,10 @@
+//OOP is a programming paradigm that is centered around objects rather than functions in procedural programming
+console.log('hello world')
+
+//The four pillars of OOP:
+//Encapsulation: grouping related variables and functions into objects
+//Abstraction: properties and methods can be hidden which simplifies the interface of the object and reduces the impact of change
+//Inheritance: it allows to eliminate redundant code by allowing for creating generic objects from which other objects inherit properties and methods
+//Polymorphism: allows for properties that behave differently depending on the object being referenced. Refactor ugly switch/case statements
+
+//OOP solves the problem of interdependecy (spaghetti code) & repitition in Procedural programming
